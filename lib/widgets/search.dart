530d1f7428da8fd751b209fccd9d2d999dec60d4
Sanaullah <sanaullah5099@gmail.com>
2025-03-12 23:14:45 +0500

    add search bar and google logo

---
+++ lib/widgets/search.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_clone/colors.dart';
+
+class Search extends StatelessWidget {
+  const Search({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Center(
+          child: Image.asset(
+            'assets/images/google-logo.png',
+            height: size.height * 0.12,
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        SizedBox(
+          width: size.width * 0.4,
+          child: TextField(
+            decoration: InputDecoration(
+              border: const OutlineInputBorder(
+                borderSide: BorderSide(color: searchBorder),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(30),
+                ),
+              ),
+              prefixIcon: SvgPicture.asset(
+                'assets/images/search-icon.svg',
+                height: 1,
+                width: 1,
+                color: searchColor,
+              ),
+              suffixIcon: SvgPicture.asset(
+                'assets/images/mic-icon.svg',
+                width: 1,
+                height: 1,
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
